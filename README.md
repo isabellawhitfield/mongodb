b111bd6d7c6512ce541500ea6a32506f5b3de9fe
Isabella WHITFIELD <isabellawhitfield@gmail.com>
2020-03-04 15:27:12 +1300

    postman error

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mongodb
-backend 
+Download this project. Place it in your vagrant www folder. Vagrant up, once vagrant is running vagrant ssh. 
+cd in to project folder. npm i into the terminal. 
--- index.js
+++ index.js
@@ -6,46 +6,96 @@ const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 const config = require('./config.json');
 const product = require('./products.json');
+const dbProduct = require('./models/products.js');
+const User = require('./models/users.js');
 
 const port = 3000;
 
-// const mongodbURI = 'mongodb+srv://UnicornSparklePrincess:magichorse@unicornsparkleprincess-p1z3y.mongodb.net/test?retryWrites=true&w=majority';
-const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}-p1z3y.mongodb.net/test?retryWrites=true&w=majority`;
+// const mongodbURI = 'mongodb+srv://UnicornSparklePrincess:magichorse@unicornsparkleprincess-p1z3y.mongodb.net/shop?retryWrites=true&w=majority';
+const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority`;
 
 
-mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> console.log('DB connected!'))
-.catch(err =>{
-    console.log(`DBConnectionError: ${err.message}`);
-});
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected!'))
+    .catch(err => {
+        console.log(`DBConnectionError: ${err.message}`);
+    });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
+db.once('open', function () {
     console.log('We are connected to mongo db');
 });
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     console.log(`${req.method} request for ${req.url}`);
     next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/allProducts', (req, res)=>{
+app.get('/allProducts', (req, res) => {
     res.json(product);
 });
 
-app.get('/products/p=:id', (req,res)=>{
+app.get('/products/p=:id', (req, res) => {
     const idParam = req.params.id;
-  
-    for (let i = 0; i < product.length; i++){
-  
-      if (idParam.toString() === product[i].id.toString()) {
-         res.json(product[i]);
-      }
+
+    for (let i = 0; i < product.length; i++) {
+
+        if (idParam.toString() === product[i].id.toString()) {
+            res.json(product[i]);
+        }
     }
-  })
+});
+
+app.post('/registerUser', (req, res) => {
+
+    User.findOne({ username: req.body.username }, (err, userResult) => {
+        if (userResult) {
+            res.send('Username already taken');
+
+        } else {
+            const hash = bcryptjs.hashSync(req.body.password);
+            const user = new User({
+                _id: new mongoose.Types.ObjectId,
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                password: hash
+            });
+            user.save().then(result => {
+                res.send(result);
+            }).catch(err => res.send(err));
+        }
+
+    })
+});
+
+app.get('/allUsers', (req,res)=>{
+    User.find().then(result =>{
+        res.send(result);
+    })
+});
+
+app.post('/loginUser', (req,res)=>{
+  User.findOne({username:req.body.username},(err,userResult)=>{
+    if (userResult){
+      if (bcryptjs.compareSync(req.body.password, userResult.password)){
+        res.send(userResult);
+      } else {
+        res.send('not authorized');
+      }//inner if
+    } else {
+       res.send('user not found. Please register');
+    }//outer if
+  });//findOne
+});//post
 
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Mongodb app listening on port ${port}!`))
